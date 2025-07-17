8e3c7066cf631c17f9b56ecf09b89fd08acedd8d
txzstxzs <18851310017@163.com.com>
2025-07-17 21:54:12 +0800

    commit

---
+++ utils/util.py
@@ -0,0 +1,177 @@
+import os
+import numpy as np
+import torch
+import random
+
+
+def flatten(v):
+    """
+    Flatten a list of lists/tuples
+    """
+
+    return [x for y in v for x in y]
+
+
+def find_max_epoch(path):
+    """
+    Find maximum epoch/iteration in path, formatted ${n_iter}.pkl
+    E.g. 100000.pkl
+
+    Parameters:
+    path (str): checkpoint path
+    
+    Returns:
+    maximum iteration, -1 if there is no (valid) checkpoint
+    """
+
+    files = os.listdir(path)
+    epoch = -1
+    for f in files:
+        if len(f) <= 4:
+            continue
+        if f[-4:] == '.pkl':
+            try:
+                epoch = max(epoch, int(f[:-4]))
+            except:
+                continue
+    return epoch
+
+
+def print_size(net):
+    """
+    Print the number of parameters of a network
+    """
+
+    if net is not None and isinstance(net, torch.nn.Module):
+        module_parameters = filter(lambda p: p.requires_grad, net.parameters())
+        params = sum([np.prod(p.size()) for p in module_parameters])
+        print("{} Parameters: {:.6f}M".format(
+            net.__class__.__name__, params / 1e6), flush=True)
+
+
+# Utilities for diffusion models
+
+def std_normal(size):
+    """
+    Generate the standard Gaussian variable of a certain size
+    """
+
+    return torch.normal(0, 1, size=size).cuda()
+
+
+def calc_diffusion_step_embedding(diffusion_steps, diffusion_step_embed_dim_in):
+    """
+    Embed a diffusion step $t$ into a higher dimensional space
+    E.g. the embedding vector in the 128-dimensional space is
+    [sin(t * 10^(0*4/63)), ... , sin(t * 10^(63*4/63)), cos(t * 10^(0*4/63)), ... , cos(t * 10^(63*4/63))]
+
+    Parameters:
+    diffusion_steps (torch.long tensor, shape=(batchsize, 1)):     
+                                diffusion steps for batch data
+    diffusion_step_embed_dim_in (int, default=128):  
+                                dimensionality of the embedding space for discrete diffusion steps
+    
+    Returns:
+    the embedding vectors (torch.tensor, shape=(batchsize, diffusion_step_embed_dim_in)):
+    """
+
+    assert diffusion_step_embed_dim_in % 2 == 0
+
+    half_dim = diffusion_step_embed_dim_in // 2
+    _embed = np.log(10000) / (half_dim - 1)
+    _embed = torch.exp(torch.arange(half_dim) * -_embed).cuda()
+    _embed = diffusion_steps * _embed
+    diffusion_step_embed = torch.cat((torch.sin(_embed),
+                                      torch.cos(_embed)), 1)
+
+    return diffusion_step_embed
+
+
+def calc_diffusion_hyperparams(T, beta_0, beta_T):
+    """
+    Compute diffusion process hyperparameters
+
+    Parameters:
+    T (int):                    number of diffusion steps
+    beta_0 and beta_T (float):  beta schedule start/end value, 
+                                where any beta_t in the middle is linearly interpolated
+    
+    Returns:
+    a dictionary of diffusion hyperparameters including:
+        T (int), Beta/Alpha/Alpha_bar/Sigma (torch.tensor on cpu, shape=(T, ))
+        These cpu tensors are changed to cuda tensors on each individual gpu
+    """
+
+    Beta = torch.linspace(beta_0, beta_T, T)  # Linear schedule
+    Alpha = 1 - Beta
+    Alpha_bar = Alpha + 0
+    Beta_tilde = Beta + 0
+    for t in range(1, T):
+        Alpha_bar[t] *= Alpha_bar[t - 1]  # \bar{\alpha}_t = \prod_{s=1}^t \alpha_s
+        Beta_tilde[t] *= (1 - Alpha_bar[t - 1]) / (
+                1 - Alpha_bar[t])  # \tilde{\beta}_t = \beta_t * (1-\bar{\alpha}_{t-1})
+        # / (1-\bar{\alpha}_t)
+    Sigma = torch.sqrt(Beta_tilde)  # \sigma_t^2  = \tilde{\beta}_t
+
+    _dh = {}
+    _dh["T"], _dh["Beta"], _dh["Alpha"], _dh["Alpha_bar"], _dh["Sigma"] = T, Beta, Alpha, Alpha_bar, Sigma
+    diffusion_hyperparams = _dh
+    return diffusion_hyperparams
+
+
+
+def get_mask_mcar(sample, mr): 
+    mask = torch.ones(sample.shape).view(1,-1) 
+    index = torch.tensor(range(mask.shape[1]))  
+    perm = torch.randperm(len(index)) 
+    index = perm[ 0: round( mr*len(index) ) ]
+    mask[0][index]=0
+    mask = mask.view(sample.shape)
+    return mask
+
+
+
+def get_mask_rm(sample, k):   
+    """Get mask of random points (missing at random) across channels based on k,
+    where k == number of data points. Mask of sample's shape where 0's to be imputed, and 1's to preserved
+    as per ts imputers"""
+
+    mask = torch.ones(sample.shape)          
+    length_index = torch.tensor(range(mask.shape[0]))    
+    
+    for channel in range(mask.shape[1]):             
+        perm = torch.randperm(len(length_index))      
+        idx = perm[0:k]                       
+        mask[:, channel][idx] = 0                
+
+    return mask
+
+
+def get_mask_mnr(sample, k):  
+    """Get mask of random segments (non-missing at random) across channels based on k,
+    where k == number of segments. Mask of sample's shape where 0's to be imputed, and 1's to preserved
+    as per ts imputers"""
+
+    mask = torch.ones(sample.shape)
+    length_index = torch.tensor(range(mask.shape[0]))
+    list_of_segments_index = torch.split(length_index, k)
+    for channel in range(mask.shape[1]):
+        s_nan = random.choice(list_of_segments_index)
+        mask[:, channel][s_nan[0]:s_nan[-1] + 1] = 0
+
+    return mask
+
+
+def get_mask_bm(sample, k):  #
+    """Get mask of same segments (black-out missing) across channels based on k,
+    where k == number of segments. Mask of sample's shape where 0's to be imputed, and 1's to be preserved
+    as per ts imputers"""
+
+    mask = torch.ones(sample.shape)
+    length_index = torch.tensor(range(mask.shape[0]))
+    list_of_segments_index = torch.split(length_index, k)
+    s_nan = random.choice(list_of_segments_index)
+    for channel in range(mask.shape[1]):
+        mask[:, channel][s_nan[0]:s_nan[-1] + 1] = 0
+
+    return mask
